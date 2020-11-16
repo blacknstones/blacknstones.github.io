17e928a6cb71e4eb2e6a06d4ccc3a5860db60270
blacknstones <blacknstones@gmail.com>
2020-11-17 00:33:51 +0100

    fixed button at homepage

--- src/components/molecules/HomeCard.jsx
+++ src/components/molecules/HomeCard.jsx
@@ -4,18 +4,17 @@ import { Link } from 'react-router-dom';
 
 export default function HomeCard() {
 
+
+
     return (
         <div className="home-card">
             <div className="home-card-content">
                 <p>Hi, I'm Yanwei</p>
-                <br />
-                <div>
-                    <i class="fas fa-arrow-right"></i>
-                </div>
-
-
-
-
+                <button className="button arrow">
+                     <i className="fas fa-arrow-right"></i>
+                </button>
+               
+                
             </div>
 
         </div>
