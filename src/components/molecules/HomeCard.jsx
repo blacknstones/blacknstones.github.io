c9d2acdde1ff0e16403c7c8eda848a3b4ba79ec5
blacknstones <blacknstones@gmail.com>
2020-11-21 23:01:53 +0100

    modified homecard

--- src/components/molecules/HomeCard.jsx
+++ src/components/molecules/HomeCard.jsx
@@ -1,6 +1,6 @@
 // React core
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 export default function HomeCard() {
 
@@ -9,7 +9,7 @@ export default function HomeCard() {
     return (
         <div className="home-card">
             <div className="home-card-content">
-                <p>Hi, I'm Yanwei</p>
+                <p>Hi,I am Yanwei</p>
                 <button className="button arrow">
                      <i className="fas fa-arrow-right"></i>
                 </button>
