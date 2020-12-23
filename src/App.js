2fe9434a27ec43d75da778427b38fb93bea64785
zhu <blacknstones@gmail.com>
2020-12-24 00:24:06 +0100

    modified import

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import HomePage from './components/templates/HomePage';
-import ProjectPage from './components/templates/ProjectPage';
-import AboutPage from './components/templates/AboutPage';
+import HomePage from './components/pages/HomePage';
+import ProjectPage from './components/pages/ProjectPage';
+import AboutPage from './components/pages/AboutPage';
 
 import './css/style.css';
 
