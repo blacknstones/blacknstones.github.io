11d81e844e83117a2893efd934a2b1010c18b9fe
zhu <blacknstones@gmail.com>
2021-02-06 01:09:16 +0100

    card component in progress

--- src/components/about/Card.tsx
+++ src/components/about/Card.tsx
@@ -5,12 +5,26 @@ type props = {
     title: string
 }
 
+const style = {
+    color: randomColor(),
+    border: '5px solid pink',
+}
+
 const Card: React.FC<props> = ({title}) => {
+
+    const [fliped, setFliped] = React.useState(false);
+    const handleFlip = () => {
+        setFliped(true);
+    }
+
     return (
     <div className="card" 
-    style={{color: randomColor()}} 
-    onClick={}>
-        {title}
+    style={style} 
+    onClick={handleFlip}>
+        {fliped ?  
+        <p className="front">{title}</p> : 
+        <p className="back">?</p>}
+        
     </div>);
 }
 
--- src/components/about/Strength.tsx
+++ src/components/about/Strength.tsx
@@ -1,30 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import pointer from "../../assets/cursor1.png";
 import Card from "./Card";
 
 const Strength: React.FC = () => {
+  const [opened, setOpened] = useState(false);
+
   return (
     <section className="about-game">
       <div className="text">
-        <p>It's 3021. You live in a time when AI has taken control. </p>
+        <p>It's 3021. A time when AI has taken control. </p>
         <p>
-          Human can no longer live independently or make decisions without
+          Human can no longer live or make decisions independently without
           machines.
         </p>
         <p>
           From life to death, every human's consciousness is uploaded to the
           cloud in real-time for AI to consume.
         </p>
-        <p>You, an awakening mind, will start the resistance with the help of your friend Zhu.</p>
+        <p>You, an awakening mind, will start the resistance with the help of your AI friend Zhu.</p>
+        <p>&rarr;</p>
       </div>
 
-      <div>
-        <p>Choose one Strength shared by Zhu:</p>
+      
+
+      <div className="choices">
+        <p className="prompt">Zhu has some pre-defined setting. Choose one:</p>
 
         <div className="cards">
            <Card title="Inqusitive"/>
            <Card title="Creative" />
-           <Card title="Love"/>
+           <Card title="Proactive"/>
         </div>
       </div>
     </section>
