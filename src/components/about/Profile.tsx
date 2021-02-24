6c59d44ef0d38efcf20a402a13d6f284817231b7
zhu <blacknstones@gmail.com>
2021-02-24 23:53:28 +0100

    upgrade tab

--- src/components/about/Profile.tsx
+++ src/components/about/Profile.tsx
@@ -6,13 +6,11 @@ import Tab from "../tabs/Tab";
 import Journey from "../profile/Journey";
 
 const Profile: React.FC = () => {
-  // const [showFirst, setShowFirst] = React.useState(false);
-  // const [showSecond, setShowSecond] = React.useState(true);
 
   return (
     <div className="content-container profile">
-      <Tabs showDefault={true} transform="clicked">
-        <Tab title="Who am I?">
+      <Tabs showDefault={true} transform="active">
+        <Tab title="1" newTitle="Who am I?">
           <div className="layout-container text">
              <p>My name is Yanwei Zhu.</p>
           <p>
@@ -21,9 +19,9 @@ const Profile: React.FC = () => {
             medium.
           </p>
           </div>
-         
         </Tab>
-        <Tab title="How did I get here?">
+
+        <Tab title="2" newTitle="How did I get here?">
           <div className="layout-container text">
              <p>
               By experimenting, self-motivating and constant learning, I
@@ -32,17 +30,17 @@ const Profile: React.FC = () => {
             </p>
             <Journey />
           </div>
-       
         </Tab>
+
+        <Tab title="3" newTitle="What's next?">
+          <div className="layout-container text">
+             <p>Interesting projects</p>
+             <p>Collaboration with motivated developers</p>
+          </div>
+        </Tab>
+
       </Tabs>
 
-      {/* {showFirst && (
-        <Intro setShowFirst={setShowFirst} setShowSecond={setShowSecond} />
-      )}
-
-      {showSecond && (
-        <Background setShowFirst={setShowFirst} setShowSecond={setShowSecond} />
-      )} */}
     </div>
   );
 };
--- src/components/tabs/TabTitle.tsx
+++ src/components/tabs/TabTitle.tsx
@@ -1,48 +1,62 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { ColorContext } from '../reusables/ColorContext';
+import React, { useCallback, useEffect, useState } from "react";
+import { ColorContext } from "../reusables/ColorContext";
 
 type Props = {
-    title: string,
-    newTitle?: string,
-    icon?: string,
-    newIcon?: string,
-    transformType: "clicked" | "active",
-    active: boolean,
-    index: number,
-    setSelectedTab: (index: number) => void
-  }
-  
-  const TabTitle: React.FC<Props> = ({ title, newTitle, icon, newIcon, index, setSelectedTab, transformType, active }) => {
-    const color = React.useContext(ColorContext);
-    const ColorStyle = {
-      color: color,
-    };
+  title: string;
+  newTitle?: string;
+  icon?: string;
+  newIcon?: string;
+  transformType: "clicked" | "active";
+  active: boolean;
+  index: number;
+  setSelectedTab: (index: number) => void;
+};
 
-    const [clicked, setClicked] = useState(false);
+const TabTitle: React.FC<Props> = ({
+  title,
+  newTitle,
+  icon,
+  newIcon,
+  index,
+  setSelectedTab,
+  transformType,
+  active,
+}) => {
+  const color = React.useContext(ColorContext);
+  const ColorStyle = {
+    color: color,
+  };
 
-    // check the transform condition 
-    const transformed: boolean = (transformType === "clicked") ? clicked : active;
+  const [clicked, setClicked] = useState(false);
 
-    const handleClick = useCallback( () => {
-      setSelectedTab(index);
-      setClicked(true);
-    }, [setSelectedTab, index]);
+  // check the transform condition
+  const transformed: boolean = transformType === "clicked" ? clicked : active;
 
-    // useEffect(() => {
-    //   console.log("inside TabTitle:", title);
-    //   console.log("transform type: ", transformType);
-    //   console.log("active:", active);
-    //   console.log("clicked:", clicked);
-    // }, []);
+  const handleClick = useCallback(() => {
+    setSelectedTab(index);
+    setClicked(true);
+  }, [setSelectedTab, index]);
 
-    return (
-      <li className="tab-title">
-        <div className="tab-button" onClick={handleClick}>
-          <img className="tab-img" src={transformed && newIcon ? newIcon : icon}/>
-          <p className="tab-text">{transformed && newTitle ? newTitle : title}</p>
-          </div>
-      </li>
-    )
-  }
-  
-  export default TabTitle;
+  // useEffect(() => {
+  //   console.log("inside TabTitle:", title);
+  //   console.log("transform type: ", transformType);
+  //   console.log("active:", active);
+  //   console.log("clicked:", clicked);
+  // }, []);
+
+  return (
+    <li className={"tab-title " + (active ? "active" : "")}>
+      <div className="tab-button" onClick={handleClick}>
+        <img
+          className="tab-icon"
+          src={transformed && newIcon ? newIcon : icon}
+        />
+        <p className="tab-text" style={active ? ColorStyle : undefined}>
+          {transformed && newTitle ? newTitle : title}
+        </p>
+      </div>
+    </li>
+  );
+};
+
+export default TabTitle;
