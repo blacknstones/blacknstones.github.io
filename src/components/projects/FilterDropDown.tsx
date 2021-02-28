fe43cf50f1a948a56248a2e79777c833a1c1596b
zhu <blacknstones@gmail.com>
2021-02-28 23:28:24 +0100

    filter in progress

--- src/components/projects/FilterDropDown.tsx
+++ src/components/projects/FilterDropDown.tsx
@@ -3,5 +3,5 @@ import * as React from 'react';
 const FilterDropDown: React.FC = () => {
     return(<div>filter button</div>);
 }
-
+  
 export default FilterDropDown;
--- src/components/projects/Filters.tsx
+++ src/components/projects/Filters.tsx
@@ -16,22 +16,79 @@ interface IProjects {
   setItems: (items: Project[]) => void;
 }
 
-const Filters: React.FC<IProjects> = ({ items, setItems }) => {
-  //find initial categories, languages and tools
-//   const initialCategories: string[] = (projectsData) => {
-//       let categories: string[] = [];
+interface Filters {
+    categories : {
+        options: string[],
+        selected: string[] | null
+    },
+    languages: {
+        options: string[],
+        selected: string[] | null
+    },
+    tools: {
+        options: string[],
+        selected: string[] | null
+    }
+}
 
-//   }
+const initializeFilters = (items: Project[]) => {
+    let filters: Filters = {
+        categories: {
+            selected: null,
+            options: []
+        },
+        languages: {
+            selected: null,
+            options: []
+        },
+        tools: {
+            selected: null,
+            options: []
+        }
+    };
+
+    items.forEach(item => {
+        item.categories.forEach(category => {
+            if (!filters.categories.options.some(option => option === category)) {
+                filters.categories.options.push(category);
+            }
+        });
+
+        item.languages.forEach(lang => {
+            if (!filters.languages.options.some(option => option === lang)) {
+                filters.languages.options.push(lang);
+            }
+        });
+
+        item.tools.forEach(tool => {
+            if (!filters.tools.options.some(option => option === tool)) {
+                filters.tools.options.push(tool);
+            }
+        })
+    });
+
+    return filters;
+
+}
+
+const Filters: React.FC<IProjects> = ({ items, setItems }) => {
+     
+  const [filters, setFilters] = useState(()=> {
+      return initializeFilters(items);
+  });
+
+
+
 
 
 
 useEffect(() => {
-    console.log(items);
+    const prop = items.map(item => {
+        Object.entries(item)
+    })
+    console.log(prop);
 },[items]);
 
-
- 
-  const [activeCategories, setActiveCategories] = useState();
 
   return (
       <div className="">
