82f2be1f281f39d24eb054c46f5d0041b66b74e6
zhu <blacknstones@gmail.com>
2021-03-02 00:06:21 +0100

    filter button

--- src/components/projects/FilterButton.tsx
+++ src/components/projects/FilterButton.tsx
@@ -1,7 +1,24 @@
 import * as React from 'react';
 
-const FilterButton: React.FC = () => {
-    return(<div>filter button</div>);
+
+  type Props = {
+    onAddFilter: (level: string, option: string) => void;
+    onRemoveFilter: (level: string, option: string) => void;
+    level: "category" | "language" | "tool";
+    option: string;
+}
+
+const FilterButton: React.FC<Props> = ({ onAddFilter, onRemoveFilter, level, option, children}) => {
+    const [active, setActive] = React.useState(false);
+
+    return(
+    <button onClick={()=> {
+        setActive(!active);
+        active ? onAddFilter(level, option) : onRemoveFilter(level, option);
+    }} >
+        {children}
+    </button>
+        );
 }
 
 export default FilterButton;
