ce203db86dac04d884923beba15471389bfdc444
zhu <blacknstones@gmail.com>
2021-03-03 18:10:54 +0100

    small typo

--- src/components/profile/Journey.tsx
+++ src/components/profile/Journey.tsx
@@ -67,7 +67,7 @@ const Journey: React.FC = () => {
             <p className="title">
                 Current
               </p>
-              <p className="detail">I'm learning new technologies everydayand building projects on my own.</p>
+              <p className="detail">I'm learning new technologies everyday and building projects on my own.</p>
               <p>Check out my projects <Link to="/projects" className="inline-link">here</Link></p>
             </ColorBox>
           </Tab>
