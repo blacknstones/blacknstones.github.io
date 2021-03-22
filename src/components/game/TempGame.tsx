fd0f2648e023abe04cf0cbc316909187aeac1a98
zhu <blacknstones@gmail.com>
2021-03-22 21:51:25 +0100

    update tempgame

--- src/components/game/TempGame.tsx
+++ src/components/game/TempGame.tsx
@@ -2,7 +2,8 @@ import * as React from "react";
 
 const TempGame: React.FC = () => {
     return (<div className="content-container">
-        <div className="layout-container">You have found a non-existing game.</div>
+        <div className="layout-container">You have found a non-existing <button>
+            game.</button> </div>
     </div>)
 }
 
